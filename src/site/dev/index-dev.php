72eb423a9aa0ffd2b149d3cc2e7b18d583fcc610
samantha.lovisolo <samantha.lovisolo@telepass.com>
2022-01-14 18:08:02 +0100

    add dev wip of header

---
+++ src/site/dev/index-dev.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>
+    </title>
+    <link href='https://fonts.googleapis.com/css?family=Share Tech Mono' rel='stylesheet'>
+    <link rel="stylesheet" href="style-dev.css">
+</head>
+
+<body>
+
+<div class="container">
+    <div class="sq">square</div>
+    <div class="sq2">test</div>
+</div>
+<!--
+<div class="anim"></div>
+<div class="anim"></div>
+<div class="anim"></div>
+<div class="anim"></div>
+<div class="anim"></div>
+-->
+</body>
+
+</html>
